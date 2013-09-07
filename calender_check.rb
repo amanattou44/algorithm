c438fcfc919c92a4676a20794fd27caddd5ba244
amanattou44 <amanattou44@gmail.com>
2013-09-07 21:58:59 +0900

    リファクタリングしました

--- calender_check.rb
+++ calender_check.rb
@@ -1,33 +1,11 @@
 require "date"
 
-file = ARGV[0]
+_, *years = ARGF.readlines.map(&:chomp).map(&:to_i)
 
-def judgement_leap(int)
-  if(Date.gregorian_leap?(int))
-    puts "#{int} is a leap year"
+years.each do |year|
+  if Date.gregorian_leap?(year)
+    puts "#{year} is a leap year"
   else
-    puts "#{int} is not a leap year"
+    puts "#{year} is not a leap year"
   end
-end
-
-unless file == nil
-  read = open(file)
-  while line = read.gets do
-    judgement_leap(line.to_i)
-  end
-  read.close
-else
-  input_line = gets.to_i
-  input_line.times {
-    s = gets.chomp.split(",")
-    if(s.length > 1)
-      s.each do |v|
-        judgement_leap(v.to_i)
-      end
-    elsif(s.length == 1)
-      judgement_leap(s.first.to_i)
-
-      return
-    end
-  }
 end
