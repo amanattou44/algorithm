10142a987e66ac882bb6f0cd4cacf22b633f1082
amanattou44 <amanattou44@gmail.com>
2013-09-08 18:47:48 +0900

    refactor

--- pv_check.rb
+++ pv_check.rb
@@ -6,9 +6,8 @@ campaign_list_max_size = logs.each_cons(campaign).with_object([]) do |arr, resul
   result << arr.inject { |a, b| a + b }
 end
 
-list_size = 0
-campaign_list_max_size.each_with_index do |val, i|
-  list_size += 1 if campaign_list_max_size.max == val
+campaign_list = campaign_list_max_size.count do |val|
+  campaign_list_max_size.max == val
 end
 
-print "#{list_size} #{campaign_list_max_size.find_index(campaign_list_max_size.max) + 1} \n"
+print "#{campaign_list} #{campaign_list_max_size.find_index(campaign_list_max_size.max) + 1} \n"
