bf0ba9ca6b7910b162b4bb6de4ef05ad026aa882
amanattou44 <amanattou44@gmail.com>
2013-09-08 18:37:37 +0900

    refactor

--- pv_check.rb
+++ pv_check.rb
@@ -1,18 +1,14 @@
-days, access_logs = ARGF.readlines.map(&:split)
+(_, day), access_logs = ARGF.readlines.map(&:split)
 logs = access_logs.map(&:to_i)
-campaign = days[1].to_i
+campaign = day.to_i
 
 campaign_list_max_size = logs.each_cons(campaign).with_object([]) do |arr, result|
   result << arr.inject { |a, b| a + b }
 end
 
 list_size = 0
-day_arr = []
 campaign_list_max_size.each_with_index do |val, i|
-  if campaign_list_max_size.max == val
-    list_size += 1
-    day_arr << i
-  end
+  list_size += 1 if campaign_list_max_size.max == val
 end
 
-print "#{list_size} #{day_arr.first + 1} \n"
+print "#{list_size} #{campaign_list_max_size.find_index(campaign_list_max_size.max) + 1} \n"
