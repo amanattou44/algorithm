a1b16b0b5d6bf001a8e81936cd0483581b38e5bc
amanattou44 <amanattou44@gmail.com>
2013-09-08 16:56:02 +0900

    refactor

--- pv_check.rb
+++ pv_check.rb
@@ -1,52 +1,20 @@
-# 入力
-# ・キャンペーンの候補数、キャンペーン日数
-# 出力
-# ・キャンペーン期間の候補数、キャンペーンの開始日
+days, logs = ARGF.readlines.map(&:split)
+days.map!(&:to_i)
+logs.map!(&:to_i)
+campaign = days[1]
 
-# 準備
-input = gets
-days, fast_day = input.chomp!.split(" ");
-days = days.to_i
-fast_day = fast_day.to_i
-
-# 一次元配列、配列の要素数、キャンペーンの日数
-def candidate(list, list_num, num)
-  lump = Array.new(list_num - 1).map { Array.new }
-  lump.each_with_index do |val, i|
-    lump[i] = list.values_at(0..num - 1)
-    list.delete_at(0)
-  end
-  lump.each_with_index do |arr, i|
-    arr.each_with_index do |val, j|
-      lump.slice!(i) if(val.nil?)
-    end
-  end
-  return lump
+campaign_list_max_size = Array.new
+logs.each_cons(campaign) do |arr|
+  campaign_list_max_size.push arr.inject { |a, b| a + b }
 end
 
-@fast_open = Array.new
-# 二次元配列
-def candidate_size(list)
-  list.each_with_index do |arr, i|
-    max_size = arr.inject { |sum, j| sum.to_i + j.to_i } if(i == 0)
-    current_size = arr.inject { |sum, j| sum.to_i + j.to_i}
-    if((current_size <=> max_size) > -1)
-      max_size = current_size
-      @fast_open = arr
-      size += 1
-    end
+list_size = 0
+day_arr = Array.new
+campaign_list_max_size.each_with_index do |val, i|
+  if campaign_list_max_size.max == val
+    list_size += 1
+    day_arr.push i
   end
-  return size
 end
 
-# 6 2 0 7 1 3 5 3 2 6
-# [6,2][2,0][0,7][7,1][1,3][3,5][5,3][3,2][2,6]
-# メイン実行
-footprint_list = gets.chomp!.split(" ")
-list = Array.new
-# キャンペーン期間の候補数
-candidate_num = 0
-
-list = candidate(footprint_list, footprint_list.length, fast_day)
-candidate_num = candidate_size list
-print "#{candidate_num} #{@fast_open.first}\n"
+print "#{list_size} #{day_arr.first + 1} \n"
